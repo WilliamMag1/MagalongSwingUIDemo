3544cad7fc4b60482eb527a3cf77cb9318c19b14
willj <williamm534@nycstudents.net>
2023-05-02 06:37:44 -0400

    Initial commit

---
+++ src/MainGUIWindow.java
@@ -0,0 +1,72 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class MainGUIWindow extends JFrame implements ActionListener, KeyListener {
+    private JPanel mainPanel;
+    private JTextArea myTextArea;
+    private JTextField textField1;
+    private JButton button1;
+    private JLabel label1;
+    private JButton button2;
+
+    public MainGUIWindow(){
+        createUIComponents();
+    }
+
+    public void createUIComponents(){
+        setContentPane(mainPanel);
+        setTitle("My GUI");
+        setSize(300,400);
+        setLocation(450,100);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        button1.addActionListener(this);
+        button2.addActionListener(this);
+        textField1.addKeyListener(this);
+        setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e){
+        Object source = e.getSource();
+        if (source instanceof JButton){
+            JButton button = (JButton) source;
+            String text = button.getText();
+
+            if (text.equals("Click if cool")){
+                myTextArea.append("button 1 clicked! ");
+            }else{
+                myTextArea.append("button 2 clicked! ");
+            }
+        }
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e){
+        Object source = e.getSource();
+        if (source instanceof JTextField){
+            myTextArea.append("Key typed");
+
+        }
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e){
+        Object source = e.getSource();
+        if (source instanceof JTextField){
+            myTextArea.append("Key pressed");
+
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e){
+        Object source = e.getSource();
+        if (source instanceof JTextField){
+            myTextArea.append("Key released");
+
+        }
+    }
+}
